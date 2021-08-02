cb3da6f75a919ad00d27167f71499354c1756119
Rushali Sarkar <rushalisarkar2000@gmail.com>
2021-08-03 02:59:46 +0530

    Added Case 202: The case of Bob and Daisy Part 3: Bob to Daisy

---
+++ 202-2-98600094-6029488/sketch.js
@@ -0,0 +1,228 @@
+/*
+
+Officer: 6029488
+CaseNum: 202-2-98600094-6029488
+
+Case 202 - The case of Bob and Daisy - stage 3
+
+Wow these two like to ham it up. Here’s the next letter. This time it’s from Bob (aka. Jobs).
+I need you to decode it to uncover more details about their plan.
+
+Discover the hidden code by commenting out all text commands except
+those which produce magenta filled text in Ballpointprint font.
+Only comment out text commands - leave fill & stroke commands uncommented.
+
+There are many possible ways of investigating this case, but you
+should use ONLY the following commands:
+
+  // comments are all that are needed for this case.
+  Do NOT add any new lines of code.
+
+*/
+
+var Ballpointprint;
+var Melissa;
+var Diggity;
+var RonsFont;
+
+
+function preload()
+{
+	Ballpointprint = loadFont('Ballpointprint.ttf');
+	Melissa = loadFont('Melissa.otf');
+	Diggity = loadFont('Diggity.ttf');
+	RonsFont = loadFont('RonsFont.ttf');
+}
+
+function setup()
+{
+	createCanvas(561,480);
+	textSize(26);
+}
+
+function draw()
+{
+	background(255);
+
+	fill(0,255,0);
+	textFont(Diggity);
+	// text("ry", 213,199);
+	textFont(Melissa);
+	// text("bar", 9,289);
+	// text("back", 248,141);
+	textFont(Ballpointprint);
+	// text("will", 192,257);
+	// text("keep", 123,171);
+	textFont(RonsFont);
+	// text("not", 499,225);
+	fill(255,0,255);
+	textFont(Ballpointprint);
+	text("old", 351,225);
+	text("harbour", 390,225);
+	text("car", 46,141);
+	text("away", 65,108);
+	text("down", 212,225);
+	text("hidden", 268,171);
+	fill(0,0,255);
+	textFont(RonsFont);
+	// text("Daisy,", 78,26);
+	// text("devotion", 9,199);
+	// text("be", 232,257);
+	fill(255,0,0);
+	textFont(Ballpointprint);
+	// text("Jerrys", 420,257);
+	// text("shall", 381,171);
+	// text("now", 124,141);
+	// text("the", 336,289);
+	textFont(Diggity);
+	// text("shooters", 161,289);
+	fill(255,0,255);
+	textFont(RonsFont);
+	// text("so", 326,108);
+	textFont(Diggity);
+	// text("in", 68,225);
+	fill(255,0,0);
+	textFont(RonsFont);
+	// text("all", 123,199);
+	fill(255,0,255);
+	textFont(Diggity);
+	// text("too", 23,78);
+	textFont(Melissa);
+	// text("I", 389,199);
+	// text("have", 61,78);
+	// text("all", 184,108);
+	fill(255,0,0);
+	// text("l", 34,26);
+	// text("isses,", 124,341);
+	// text("toasts", 334,257);
+	fill(0,0,0);
+	textFont(RonsFont);
+	// text("opport", 242,199);
+	// text("play", 82,289);
+	textFont(Melissa);
+	// text("broadcast", 431,171);
+	// text("and", 167,141);
+	// text("no", 46,171);
+	textFont(Diggity);
+	// text("our", 181,171);
+	// text("begun", 99,78);
+	textFont(RonsFont);
+	// text("dream", 403,199);
+	textFont(Ballpointprint);
+	// text("and", 37,289);
+	textFont(Melissa);
+	// text("our", 512,199);
+	fill(255,0,0);
+	// text("shall", 9,171);
+	// text("Oh", 6,26);
+	textFont(RonsFont);
+	// text("run", 196,141);
+	fill(255,0,255);
+	textFont(Ballpointprint);
+	text("get", 19,108);
+	fill(0,0,255);
+	// text("I", 460,78);
+	// text("I", 256,108);
+	// text("love", 220,171);
+	textFont(RonsFont);
+	// text("of", 481,199);
+	// text("down", 242,289);
+	// text("down", 253,78);
+	fill(255,0,0);
+	// text("cou", 155,78);
+	// text("We", 347,171);
+	fill(0,0,0);
+	textFont(Diggity);
+	// text("Upon", 369,141);
+	// text("and", 173,225);
+	fill(0,0,255);
+	// text("wine", 499,257);
+	// text("at", 390,257);
+	// text("our", 508,171);
+	// text("x", 57,393);
+	// text("at", 306,289);
+	fill(0,0,0);
+	textFont(RonsFont);
+	// text("my", 9,141);
+	fill(255,0,255);
+	textFont(Ballpointprint);
+	text("by", 268,225);
+	textFont(RonsFont);
+	// text("to", 285,141);
+	textFont(Melissa);
+	// text("we", 165,257);
+	// text("to", 424,108);
+	// text("I", 9,78);
+	fill(0,255,0);
+	textFont(Ballpointprint);
+	// text("return", 442,141);
+	// text("in", 514,108);
+	fill(255,0,0);
+	textFont(RonsFont);
+	// text("Soon", 109,257);
+	fill(0,0,255);
+	textFont(Melissa);
+	// text("ove", 37,26);
+	// text("tempted", 357,108);
+	fill(0,0,0);
+	// text("the", 90,225);
+	fill(255,0,255);
+	textFont(Ballpointprint);
+	text("the", 306,225);
+	textFont(Melissa);
+	// text("my", 416,141);
+	// text("woods", 120,225);
+	fill(0,0,255);
+	// text("ly", 61,26);
+	// text("k", 116,341);
+	// text("the", 317,78);
+	fill(0,255,0);
+	textFont(RonsFont);
+	// text("this.", 204,108);
+	fill(0,0,255);
+	// text("at", 154,199);
+	// text("nting", 193,78);
+	fill(255,0,255);
+	textFont(Diggity);
+	// text("walks", 11,225);
+	fill(0,0,255);
+	textFont(Ballpointprint);
+	// text("days", 347,78);
+	// text("!", 86,257);
+	// text("we", 507,141);
+	textFont(RonsFont);
+	// text("arcade.", 381,289);
+	// text("and", 65,341);
+	textFont(Ballpointprint);
+	// text("raising", 263,257);
+	textFont(RonsFont);
+	// text("am", 287,108);
+	textFont(Melissa);
+	// text("can", 491,78);
+	fill(0,255,0);
+	// text("right", 84,141);
+	// text("to", 101,199);
+	textFont(Ballpointprint);
+	// text("Bob", 6,393);
+	textFont(RonsFont);
+	// text("jump", 446,108);
+	// text("you.", 319,141);
+	// text("until", 402,78);
+	fill(0,0,0);
+	// text("ing", 124,289);
+	// text("from", 120,108);
+	textFont(Diggity);
+	// text("despair", 17,257);
+	// text("Do", 467,225);
+	// text("eve", 186,199);
+	// text("longer", 67,171);
+	fill(0,255,0);
+	textFont(Ballpointprint);
+	// text("Love", 9,341);
+	fill(0,0,0);
+	textFont(RonsFont);
+	// text("unity.", 317,199);
+
+
+
+}
